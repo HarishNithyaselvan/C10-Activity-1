d1417c78e9be602dd0d7a3633a030be9980ef7f6
HarishNithyaselvan <117954345+HarishNithyaselvan@users.noreply.github.com>
2022-11-15 17:24:40 -0500

    Add files via upload

---
+++ pro-c10-sa1-template-main/sketch.js
@@ -0,0 +1,16 @@
+
+function setup() 
+{
+  createCanvas(400,400);
+var weight = [56,97,82,43,91]
+var sum = weight[0]+weight[1]+weight[2]+weight[3]+weight[4]
+var average = sum/weight.length
+console.log(average)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
